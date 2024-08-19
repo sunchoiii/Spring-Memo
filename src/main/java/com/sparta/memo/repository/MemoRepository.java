9f0cd816c644aa3a21e1f411fdf842268b1fd53e
Sun choi <csun2772@naver.com>
2024-08-19 17:48:58 +0900

    Query Method

--- src/main/java/com/sparta/memo/repository/MemoRepository.java
+++ src/main/java/com/sparta/memo/repository/MemoRepository.java
@@ -4,8 +4,14 @@ package com.sparta.memo.repository;
 import com.sparta.memo.entity.Memo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 // 심플레포지토리에 Repository 가 달려있어서 따로 애너테이션 안해서 됨
 public interface MemoRepository extends JpaRepository<Memo, Long> {
 
+    List<Memo> findAllByOrderByModifiedAtDesc();
+
+    // 이렇게 파라변수 넣어서 동적으로 생성도 가능
+    // List<Memo> findAllByUsername(String username);
 
 }
